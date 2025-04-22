b742f5b8a49c1c5c63c557769ddc347922051161
GhostWalker562 <43276017+GhostWalker562@users.noreply.github.com>
2025-04-22 16:08:37 -0700

    Default ed25519 and secp256k1 private key's __str__ to AIP80

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to the Aptos Python SDK will be captured in this file. This changelog is written by hand for now.
 
+## Unreleased
+
+- **[Breaking Change]**: `ed25519` and `secp256k1` private key's `__str__` will now return the AIP-80 compliant string
+
 ## 0.11.0
 
 - `PrivateKey.format_private_key` can now format a AIP-80 compliant private key
--- aptos_sdk/ed25519.py
+++ aptos_sdk/ed25519.py
@@ -26,7 +26,7 @@ class PrivateKey(asymmetric_crypto.PrivateKey):
         return self.key == other.key
 
     def __str__(self):
-        return self.hex()
+        return self.aip80()
 
     @staticmethod
     def from_hex(value: str | bytes, strict: bool | None = None) -> PrivateKey:
@@ -324,6 +324,10 @@ class Test(unittest.TestCase):
             private_key_with_prefix.hex(),
             private_key_bytes.hex(),
         )
+
+    def test_private_key_aip80_formatting(self):
+        private_key_with_prefix = "ed25519-priv-0x4e5e3be60f4bbd5e98d086d932f3ce779ff4b58da99bf9e5241ae1212a29e5fe"
+        self.assertEqual(str(PrivateKey.from_str(private_key_with_prefix, True)), private_key_with_prefix)
 
     def test_sign_and_verify(self):
         in_value = b"test_message"
